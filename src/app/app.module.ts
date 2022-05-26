50b2e16f45fad976d325eaeaf6ba1e4b1a35e7ee
SCH0LL <michi.scholl@live.de>
2022-05-26 10:17:02 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { MsalModule } from '@azure/msal-angular';
+import { PublicClientApplication } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule.forRoot(new PublicClientApplication({
+      auth: {
+        clientId: '19181d06-e13d-40f4-9b73-13d85d38e441',
+        authority: 'https://login.microsoftonline.com/common',
+        redirectUri: 'http://localhost:4200'
+      },
+      cache: {
+        cacheLocation: 'LocalStorage',
+        storeAuthStateInCookie: isIE,
+      }
+    }), null, null)
   ],
   providers: [],
   bootstrap: [AppComponent]
